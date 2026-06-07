5d6c70a0045865990c8f6b6cb64152d38502bc04
jacobiwoop <jacobiwoop@gmail.com>
2026-06-07 16:30:55 +0200

    Apply Firebase notification color fix after prebuild

--- bizo-mobile-rn/plugins/with-firebase-notification-color-replace.js
+++ bizo-mobile-rn/plugins/with-firebase-notification-color-replace.js
@@ -1,26 +1,39 @@
-const { createRunOncePlugin, withAndroidManifest } = require("@expo/config-plugins");
+const fs = require("fs");
+const path = require("path");
+const { createRunOncePlugin, withDangerousMod } = require("@expo/config-plugins");
 
 const PLUGIN_NAME = "with-firebase-notification-color-replace";
 const META_DATA_NAME = "com.google.firebase.messaging.default_notification_color";
 
 function withFirebaseNotificationColorReplace(config) {
-  return withAndroidManifest(config, (config) => {
-    const application = config.modResults.manifest.application?.[0];
+  return withDangerousMod(config, [
+    "android",
+    async (config) => {
+      const manifestPath = path.join(
+        config.modRequest.platformProjectRoot,
+        "app/src/main/AndroidManifest.xml"
+      );
 
-    if (!application) {
+      if (!fs.existsSync(manifestPath)) {
+        return config;
+      }
+
+      const contents = fs.readFileSync(manifestPath, "utf8");
+
+      if (!contents.includes(META_DATA_NAME) || contents.includes(`${META_DATA_NAME}" tools:replace=`)) {
+        return config;
+      }
+
+      const next = contents.replace(
+        /(<meta-data android:name="com\.google\.firebase\.messaging\.default_notification_color" android:resource="[^"]+")(\s*\/>)/,
+        '$1 tools:replace="android:resource"$2'
+      );
+
+      fs.writeFileSync(manifestPath, next);
+
       return config;
-    }
-
-    const metaData = application["meta-data"]?.find(
-      (entry) => entry.$?.["android:name"] === META_DATA_NAME
-    );
-
-    if (metaData?.$) {
-      metaData.$["tools:replace"] = "android:resource";
-    }
-
-    return config;
-  });
+    },
+  ]);
 }
 
 module.exports = createRunOncePlugin(
